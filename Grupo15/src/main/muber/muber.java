5f2e1476d1c2ee684ab4f052bad1a0bbe05f7bfd
Erwin <Erwin@DESKTOP-0D3IOUK>
2017-04-09 02:27:51 -0300

    Carpetas organizadas

--- Grupo15/src/main/muber/muber.java
+++ Grupo15/src/main/muber/muber.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 public class muber {
 
 	public static void main(String[] args) {
-		Configuration cfg = new Configuration().configure();
+		Configuration cfg = new Configuration().configure("/main/java/hibernate/hibernate.cfg.xml");
 		SessionFactory sf = cfg.buildSessionFactory();
 	}
 
